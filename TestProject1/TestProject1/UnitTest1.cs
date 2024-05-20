20499c3dfa6713f63a2c73f07e0bf77ca3ad93fe
mishra4amit4@gmail.com <mishra4amit4@gmail.com>
2024-05-20 23:55:11 +0530

    MS 7

--- TestProject1/TestProject1/UnitTest1.cs
+++ TestProject1/TestProject1/UnitTest1.cs
@@ -1,18 +1,40 @@
+using System;
+using System.IO;
 using log4net;
 using log4net.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [assembly: XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        private readonly ILog logger = LogManager.GetLogger(typeof(UnitTest1));
+        private static readonly ILog logger = LogManager.GetLogger(typeof(UnitTest1));
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            // Ensure log4net is configured
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
-            Console.WriteLine("Hello World");
-            logger.Info("this is the test");
+            using (var consoleOutput = new StringWriter())
+            {
+                Console.SetOut(consoleOutput);
+                Console.WriteLine("Hello World");
+                logger.Info("This is the test");
+
+                string output = consoleOutput.ToString();
+                logger.Info("Console output: " + output);
+
+                // Optionally, write to debug output
+                System.Diagnostics.Debug.WriteLine(output);
+            }
         }
     }
 }
